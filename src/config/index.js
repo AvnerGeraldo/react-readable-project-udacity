ff87ab38bae33f2d1ced77305fdac14cac2abfec
Avner <avner.conceicao@hotmail.com>
2018-10-06 12:01:37 +0000

    Adicionando 'redux-saga'

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,17 @@
 import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
+//Sagas
+import rootSaga from '../sagas/rootSaga'
 
 //Reducer
 import reducer from '../reducer'
 
 //Create Store
-const store = createStore(reducer, applyMiddleware())
+const sagaMiddleware = createSagaMiddleware()
+const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+
+//Run saga
+sagaMiddleware.run(rootSaga)
+
 export default store
