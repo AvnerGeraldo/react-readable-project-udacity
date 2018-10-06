682766851fdb8b77255d35b39eb5f3f3b6039222
Avner <avner.conceicao@hotmail.com>
2018-10-06 19:14:15 +0000

    Criando 'PrivateRoute'

---
+++ src/components/Helpers/PrivateRoute.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+
+const PrivateRoute = ({ component: Component, isLogged, ...rest }) => (
+    <Route
+        {...rest}
+        render={props => (isLogged || localStorage.getItem('author')) ? (
+            <Component {...props} />
+        ) : (
+            <Redirect
+                to={{
+                    pathname: "/",
+                    state: {from: props.location},
+                }}
+            />
+        )} />
+)
+
+const mapStateToProps = state => ({
+    isLogged: state.login.isLogged
+})
+
+const mapDispatchToProps = dispatch => ({
+    verifyIsLogged: _ => dispatch({ type: 'IS_LOGGED' }),
+    logout: _ => dispatch({ type: 'LOGOUT' })
+})
+
+//Enable PrivateRoute to re render component
+export default connect(mapStateToProps, mapDispatchToProps, null, { pure: false })(PrivateRoute)
