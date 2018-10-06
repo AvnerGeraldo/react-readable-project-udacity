68aa1c95f58b60c4848fbeb28c6410dddc93b33b
Avner <avner.conceicao@hotmail.com>
2018-10-06 13:45:59 +0000

    Criando busca por 'localStorage' quando author existir

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,19 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 //Material UI
+//Structure
+import Paper from '@material-ui/core/Paper';
+
+//Items
 import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
+
+//Icons
+import PersonIcon from '@material-ui/icons/Person'
+
+//Form
 import FormControl from '@material-ui/core/FormControl';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-import Paper from '@material-ui/core/Paper';
+
+//Fonts
 import Typography from '@material-ui/core/Typography';
+
+//Core
 import withStyles from '@material-ui/core/styles/withStyles';
-import PersonIcon from '@material-ui/icons/Person'
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 //Styles
@@ -37,6 +49,16 @@ const styles = theme => ({
       alignItems: 'center',
       padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme.spacing.unit * 3}px`,
     },
+    paperError: {
+        display: 'block',
+        padding: theme.spacing.unit,
+        marginTop: theme.spacing.unit * 2,
+        alignItems: 'stretch',
+        flexDirection: 'row',
+        backgroundColor: '#FFEBEE',
+        color: '#B71C1C',
+        borderColor: '#EF9A9A'
+    },
     avatar: {
       margin: theme.spacing.unit,
       backgroundColor: theme.palette.primary.main,
@@ -50,42 +72,65 @@ const styles = theme => ({
     },
 });
 
-const Login = (props) => {
-    const { classes } = props
+class Login extends Component {
+    state = {
+        errorMsg: ''
+    }
 
-    return (
-        <React.Fragment>
-            <CssBaseline>
-                <div className={classes.layout}>
-                    <Paper className={classes.paper}>
-                        <Avatar className={classes.avatar}>
-                            <PersonIcon fontSize='large'/>
-                        </Avatar>
-                        <Typography variant="headline">Sign in</Typography>
-                        <form className={classes.form}>
-                            <FormControl margin="normal" required fullWidth>
-                                <InputLabel htmlFor="author">Author</InputLabel>
-                                <Input name="author" autoComplete="author" autoFocus />
-                            </FormControl>
-                            <FormControlLabel
-                                control={<Checkbox value="remember" color="primary" />}
-                                label="Remember me"
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="raised"
-                                color="primary"
-                                className={classes.submit}
-                            >
-                                Sign in
-                            </Button>
-                        </form>
-                    </Paper>
-                </div>
-            </CssBaseline>
-        </React.Fragment>
-    )
+    componentWillMount() {
+        new Promise((resolve) => {
+            this.props.verifyIsLogged()
+        })
+        .catch(error => this.setState({ errorMsg: error }))
+    }
+
+    render() {
+        const { classes } = this.props
+        const { errorMsg } = this.state
+
+        return (
+            <React.Fragment>
+                <CssBaseline>
+                    <div className={classes.layout}>
+                        <Paper className={classes.paper}>
+                            <Avatar className={classes.avatar}>
+                                <PersonIcon fontSize='large'/>
+                            </Avatar>
+                            <Typography variant="headline">Sign in</Typography>
+                            <form className={classes.form}>
+                                <FormControl margin="normal" required fullWidth>
+                                    <InputLabel htmlFor="author">Author</InputLabel>
+                                    <Input name="author" autoComplete="author" autoFocus />
+                                </FormControl>
+                                <FormControlLabel
+                                    control={<Checkbox value="remember" color="primary" />}
+                                    label="Remember me"
+                                />
+                                <Button
+                                    type="submit"
+                                    fullWidth
+                                    variant="raised"
+                                    color="primary"
+                                    className={classes.submit}
+                                >
+                                    Sign in
+                                </Button>
+                            </form>
+                            {errorMsg && <Paper elevation={1} className={classes.paperError}>{errorMsg}</Paper>}
+                        </Paper>
+                    </div>
+                </CssBaseline>
+            </React.Fragment>
+        )
+    }
 }
 
-export default connect(null, null)(withStyles(styles)(Login))
+const mapStateToProps = ({ login }) => ({
+    login
+})
+
+const mapDispatchToProps = dispatch => ({
+    verifyIsLogged: _ => dispatch({ type: 'IS_LOGGED' })
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Login))
