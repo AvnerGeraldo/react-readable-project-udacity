6279116a40a2f969477ca812881927d5b1303e94
Avner <avner.conceicao@hotmail.com>
2018-10-09 21:48:46 +0000

    Criando 'subHeader' para post

--- src/components/Blog/BoxPost/SubHeader.js
+++ src/components/Blog/BoxPost/SubHeader.js
@@ -2,14 +2,19 @@ import React from 'react'
 import moment from 'moment'
 import PropTypes from 'prop-types'
 
+//Helpers
+import capitalize from '../../../helpers/capitalize'
+
 const SubHeader = (props) => {
-    const { category, timestampPost } = props
-    const dateTimePost = moment.unix(timestampPost)
+    const { author, category, timestampPost } = props
+    const dateTimePost = moment(timestampPost)
     return (
         <div>
             {dateTimePost.format('MMMM, D')} of {dateTimePost.format('YYYY')} at {dateTimePost.format('HH:mm:ss')} hs
             <br />
             Category: {category}
+            <br />
+            Author: {capitalize(author)}
         </div>
     )
 }
@@ -17,6 +22,7 @@ const SubHeader = (props) => {
 const { string, number } = PropTypes
 
 SubHeader.propTypes = {
+    author: string.isRequired,
     category: string.isRequired,
     timestampPost: number.isRequired
 }
