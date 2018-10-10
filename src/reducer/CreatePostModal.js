5f27958e7a6efab37a2e51d9be6f380d47f345c1
Avner <avner.conceicao@hotmail.com>
2018-10-10 04:27:35 +0000

    Criando reducer de create post modal

---
+++ src/reducer/CreatePostModal.js
@@ -0,0 +1,32 @@
+import { OPEN_CREATE_POST_MODAL_ASYNC, CLOSE_CREATE_POST_MODAL_ASYNC, FAIL_SAVE_CREATE_POST_ASYNC } from '../actions/CreatePostModal'
+
+const initialState = {
+    openModal: false,
+    savePost: {}
+}
+
+export default (state = initialState, { type, payload }) => {
+    switch(type) {
+        case OPEN_CREATE_POST_MODAL_ASYNC:
+            return {
+                ...state,
+                openModal: true
+            }
+        case CLOSE_CREATE_POST_MODAL_ASYNC: {
+            return {
+                ...state,
+                openModal: false
+            }
+        }
+        case FAIL_SAVE_CREATE_POST_ASYNC: {
+            return {
+                ...state,
+                savePost: {
+                    error: payload.error
+                }
+            }
+        }          
+        default:
+            return state
+    }
+}
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -4,9 +4,11 @@ import { combineReducers } from 'redux'
 import login from './Login'
 import posts from './Posts'
 import filters from './PostsFilter'
+import createPostModal from './CreatePostModal'
 
 export default combineReducers({
     login,
     posts,
     filters,
+    createPostModal
 })
