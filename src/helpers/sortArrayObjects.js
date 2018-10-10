678d58fb1c3f95ad2a8f7441eda68f454c666741
Avner <avner.conceicao@hotmail.com>
2018-10-10 01:32:08 +0000

    Ajustando erro do 'localCompare' de is not a function

--- src/helpers/sortArrayObjects.js
+++ src/helpers/sortArrayObjects.js
@@ -3,7 +3,9 @@ export const sortToOldest = (dataPost, column) => {
     const dataToOrder = objKeys.map((keys) => dataPost[keys])
     const byColumn = dataToOrder.slice(0)
     
-    return byColumn.sort((a,b) => a[column] + b[column])
+    return byColumn.sort((a,b) => {
+        console.log( a[column], b[column])
+        return a[column] + b[column] || a[column].localeCompare(b[column], undefined, {sensitivity: 'base'})})
 }
 
 export const sortToNewest = (dataPost, column) => {
@@ -11,5 +13,9 @@ export const sortToNewest = (dataPost, column) => {
     const dataToOrder = objKeys.map((keys) => dataPost[keys])
     const byColumn = dataToOrder.slice(0)
     
-    return byColumn.sort((a,b) => a[column] - b[column])
+    return byColumn.sort((a,b) => {
+        if (a[column] === b[column]) {
+            return 1
+        }
+        return a[column] - b[column] || a[column].toString().localeCompare(b[column], undefined, {sensitivity: 'base'})})
 }
