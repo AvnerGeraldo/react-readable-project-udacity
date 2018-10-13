52fa92688c8c2bd95a94527dfaa8e0cf943a0b01
Avner <avner.conceicao@hotmail.com>
2018-10-13 13:37:59 +0000

    Criando loading

---
+++ src/components/Helpers/BoxShowInfoData.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+//Material UI
+//Structure
+import Grid from '@material-ui/core/Grid'
+
+//Core
+import withStyles from '@material-ui/core/styles/withStyles';
+
+//Items
+import Typography from '@material-ui/core/Typography'
+
+const styles = theme => ({
+    root: {
+        padding: theme.spacing.unit * 2,
+        marginTop: theme.spacing.unit * 0.2
+    },
+    loading: {
+        padding: theme.spacing.unit * 4,
+        marginTop: theme.spacing.unit * 10,
+        marginBottom: theme.spacing.unit * 10
+    },
+})
+
+const LoadingData = ({ textToShow, classes }) => (
+    <Grid item sm={12} xs={12} className={classes.root}>
+        <Typography variant="display1" align="center" className={classes.loading}>{textToShow}</Typography>
+    </Grid>
+)
+
+export default withStyles(styles)(LoadingData)
